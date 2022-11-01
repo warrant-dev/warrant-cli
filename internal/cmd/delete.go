f61a5ca6f39324a3a23159ef93ccc7cc038c83f3
Aditya Kajla <akajla09@gmail.com>
2022-11-01 14:18:32 -0700

    Add the ability to delete user by id to the delete cmd

--- internal/cmd/delete.go
+++ internal/cmd/delete.go
@@ -12,20 +12,21 @@ func init() {
 }
 
 var deleteCmd = &cobra.Command{
-	Use:   "delete role|tenant|permission ID",
+	Use:   "delete role|tenant|permission|user ID",
 	Short: "Delete a resource given its type and id",
-	Long:  "Delete a resource by id, specifically a role, tenant or permission.",
+	Long:  "Delete a resource by id, specifically a role, tenant, permission or user.",
 	Example: `
 warrant delete role admin-1
 warrant delete tenant tenant-2
-warrant delete permission perm-1`,
+warrant delete permission perm-1
+warrant delete user user-1`,
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) != 2 {
 			return fmt.Errorf("accepts 2 args, received %d", len(args))
 		}
 		entityType := args[0]
-		if entityType != "role" && entityType != "tenant" && entityType != "permission" {
-			return fmt.Errorf("entity to delete must be a role|tenant|permission")
+		if entityType != "role" && entityType != "tenant" && entityType != "permission" && entityType != "user" {
+			return fmt.Errorf("entity to delete must be a role|tenant|permission|user")
 		}
 		return nil
 	},
@@ -43,6 +44,8 @@ warrant delete permission perm-1`,
 			err = client.DeleteTenant(entityId)
 		case "permission":
 			err = client.DeletePermission(entityId)
+		case "user":
+			err = client.DeleteUser(entityId)
 		default:
 			return fmt.Errorf("Invalid delete request")
 		}
