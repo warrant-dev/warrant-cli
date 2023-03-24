8b37a6d7a1dab72e50f1fc3f8e1d6fca1c09bdf9
Aditya Kajla <akajla09@users.noreply.github.com>
2023-03-24 15:15:22 -0700

    Add missing list, create and delete cmds (#14)

--- internal/cmd/list.go
+++ internal/cmd/list.go
@@ -6,9 +6,12 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/warrant-dev/warrant-cli/internal/config"
 	"github.com/warrant-dev/warrant-go/v3"
+	"github.com/warrant-dev/warrant-go/v3/feature"
 	"github.com/warrant-dev/warrant-go/v3/permission"
+	"github.com/warrant-dev/warrant-go/v3/pricingtier"
 	"github.com/warrant-dev/warrant-go/v3/role"
 	"github.com/warrant-dev/warrant-go/v3/tenant"
+	"github.com/warrant-dev/warrant-go/v3/user"
 )
 
 func init() {
@@ -16,15 +19,15 @@ func init() {
 }
 
 var listCmd = &cobra.Command{
-	Use:   "list roles|tenants|permissions",
+	Use:   "list [type]",
 	Short: "List resources of built-in object types",
-	Long:  "Get a list of all resources of a given built-in object type, specifically roles, tenants and permissions.",
+	Long:  "Get a list of all resources of a given built-in object type, specifically users, tenants, roles, permissions, pricing-tiers and features.",
 	Example: `
 warrant list roles
 warrant list tenants
 warrant list permissions`,
 	Args:      cobra.ExactValidArgs(1),
-	ValidArgs: []string{"roles", "tenants", "permissions"},
+	ValidArgs: []string{"users", "tenants", "roles", "permissions", "pricing-tiers", "features"},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		err := config.InitClient()
 		if err != nil {
@@ -36,6 +39,28 @@ warrant list permissions`,
 		}
 		requestedType := args[0]
 		switch requestedType {
+		case "users":
+			users, err := user.ListUsers(&warrant.ListUserParams{
+				ListParams: listParams,
+			})
+			if err != nil {
+				return err
+			}
+			for _, user := range users {
+				fmt.Println(user.UserId)
+			}
+			return nil
+		case "tenants":
+			tenants, err := tenant.ListTenants(&warrant.ListTenantParams{
+				ListParams: listParams,
+			})
+			if err != nil {
+				return err
+			}
+			for _, tenant := range tenants {
+				fmt.Println(tenant.TenantId)
+			}
+			return nil
 		case "roles":
 			roles, err := role.ListRoles(&warrant.ListRoleParams{
 				ListParams: listParams,
@@ -47,17 +72,6 @@ warrant list permissions`,
 				fmt.Println(role.RoleId)
 			}
 			return nil
-		case "tenants":
-			tenants, err := tenant.ListTenants(&warrant.ListTenantParams{
-				ListParams: listParams,
-			})
-			if err != nil {
-				return err
-			}
-			for _, tenant := range tenants {
-				fmt.Printf("%s: %s\n", tenant.TenantId, tenant.Name)
-			}
-			return nil
 		case "permissions":
 			permissions, err := permission.ListPermissions(&warrant.ListPermissionParams{
 				ListParams: listParams,
@@ -67,6 +81,28 @@ warrant list permissions`,
 			}
 			for _, permission := range permissions {
 				fmt.Println(permission.PermissionId)
+			}
+			return nil
+		case "pricing-tiers":
+			pricingTiers, err := pricingtier.ListPricingTiers(&warrant.ListPricingTierParams{
+				ListParams: listParams,
+			})
+			if err != nil {
+				return err
+			}
+			for _, pricingTier := range pricingTiers {
+				fmt.Println(pricingTier.PricingTierId)
+			}
+			return nil
+		case "features":
+			features, err := feature.ListFeatures(&warrant.ListFeatureParams{
+				ListParams: listParams,
+			})
+			if err != nil {
+				return err
+			}
+			for _, feature := range features {
+				fmt.Println(feature.FeatureId)
 			}
 			return nil
 		default:
